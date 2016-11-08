46730e4a2e2d2446dd002b04437b8eb59b23bb60
Mark Omo <mark@markomo.me>
2016-11-07 19:10:52 -0700

    Fixed formatting to be inline with google c conventions (command) pplint.py --extensions=c,h --filter=-whitespace/tab,-legal/copyright,-readability/casting,-runtime/threadsafe_fn,-build/include deck_functions.c deck_functions.h main.c

--- deck_functions.h
+++ deck_functions.h
@@ -1,3 +1,7 @@
+#ifndef DECK_FUNCTIONS_H_
+#define DECK_FUNCTIONS_H_
+
+// Creates a card type including a suit, face value, and pointer
 typedef struct card_s {
 	char suit;
 	int face;
@@ -8,6 +12,8 @@ void create_deck(card *deck[]);
 void shuffle_deck2(card *deck[]);
 void shuffle_deck(card *deck[]);
 void print_deck(card *deck[]);
-void card_insertAfter(card *thisCard,card *newCard);
-void card_create(card *thisCard,char inSuit,int inFace,card *nextCard);
+void card_insertAfter(card *thisCard, card *newCard);
+void card_create(card *thisCard, char inSuit, int inFace, card *nextCard);
 void swap_cards(card* card1, card* card2);
+
+#endif  // DECK_FUNCTIONS_H_
--- main.c
+++ main.c
@@ -1,13 +1,51 @@
-#include<stdio.h>
 #include "deck_functions.h"
+#include <stdlib.h>
+#include <string.h>
+#include<stdio.h>
 
+typedef struct player_s {
+	char name[100];
+	float bank_roll;
+	struct player_s *next;
+} player;
 
-int main(void){
+int main(void) {
 	int numOfPlayers, i;
-	
+	player * head = malloc(sizeof(struct player_s));
+	head->next = NULL;
+	player * curr;
+	card * deck[52];
+
 	printf("Welcome to Acey Deucy!\n\nPlease enter the number of players: ");
 	scanf("%d", &numOfPlayers);
-	card* deck[52];
+	if (numOfPlayers < 1) {
+		printf("Cannot have 0 players try again");
+		exit(0);
+	}
+
+	curr = head;
+	for (int i = 1; i < numOfPlayers+1; i++) {
+		// Initilize current node
+		curr->next = malloc(sizeof(struct player_s));
+		curr = curr->next;
+		curr->next = NULL;
+		// Get Player input
+		char name[100];
+		float bank_roll;
+		printf("\nEnter player %d's name: ", i);
+		scanf("%100s", name);
+		printf("Enter player %d's bank ammount: ", i);
+		scanf("%f", &bank_roll);
+		// Set Values
+		strncpy(curr->name, name, 100);
+		curr->bank_roll = bank_roll;
+	}
+	curr = head;
+	while(curr->next != NULL) {
+		curr = curr->next;
+		printf("Player name: %s, Player cash: $%.2f\n", curr->name, curr->bank_roll);
+	}
+
 	create_deck(deck);
 	shuffle_deck(deck);
 	print_deck(deck);
